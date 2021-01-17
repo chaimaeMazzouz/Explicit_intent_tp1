5191b23ba102cddf5f5b4ba5eca60bb0c6efd5a9
chaimaeMazzouz <mazzouz.chaimae.dev@gmail.com>
2021-01-17 22:33:18 +0100

    still working

---
+++ app/src/main/java/com/example/intent_tp1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.intent_tp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_start;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_start= findViewById(R.id.buttonActivity1);
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Activity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
