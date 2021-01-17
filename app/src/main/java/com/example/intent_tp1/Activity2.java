89bab85c75b98b4b0f0d1ea7d97cc4bad8c47149
chaimaeMazzouz <mazzouz.chaimae.dev@gmail.com>
2021-01-17 23:08:09 +0100

    done

--- app/src/main/java/com/example/intent_tp1/Activity2.java
+++ app/src/main/java/com/example/intent_tp1/Activity2.java
@@ -2,13 +2,21 @@ package com.example.intent_tp1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
 
+    TextView tv_nom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        tv_nom =findViewById(R.id.textViewVl);
+        Intent i =getIntent();
+        String name = i.getStringExtra("nom");
+        tv_nom.setText(name);
     }
 }
--- app/src/main/java/com/example/intent_tp1/MainActivity.java
+++ app/src/main/java/com/example/intent_tp1/MainActivity.java
@@ -6,20 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_start;
+    EditText et_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_start= findViewById(R.id.buttonActivity1);
+        et_name = findViewById(R.id.editTextName);
 
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),Activity2.class);
+                intent.putExtra("nom",et_name.getText().toString());
                 startActivity(intent);
             }
         });
